799aa6d88eb0bdb00441899a50a4970d2c5529d5
UnknownOne76 <ganboldgantor14@gmail.com>
2023-09-08 17:49:27 +0800

    end

---
+++ src/mock/home.ts
@@ -0,0 +1,93 @@
+import leadPage from '@/images/leadpage.png';
+import leadList from '@/images/alumni.jpg';
+import leadLogo from '@/images/lead.png';
+import figma from '@/images/figma.png';
+import shagai from '@/images/shagai.png';
+import multi from '@/images/multi.png';
+import multip from '@/images/multip.png';
+import calculator from '@/images/calc.png';
+import beacon from '@/images/iron.png';
+import snake from '@/images/snake.png';
+import lead from '@/images/lead/lead.png';
+import lead1 from '@/images/lead/lead1.png';
+import lead2 from '@/images/lead/lead2.png';
+import lead3 from '@/images/lead/lead3.png';
+import lead4 from '@/images/lead/lead4.png';
+import lead5 from '@/images/lead/lead5.png';
+import demo from '@/images/shagai/demo.png';
+import demo1 from '@/images/shagai/demo1.png';
+import demo2 from '@/images/shagai/demo2.png';
+import fast from '@/images/shagai/fast.png';
+import fast1 from '@/images/shagai/fast1.png';
+import fast2 from '@/images/shagai/fast2.png';
+import fast3 from '@/images/shagai/fast3.png';
+import fast4 from '@/images/shagai/fast4.png';
+import short from '@/images/short.png';
+import short1 from '@/images/short1.png';
+import chat from '@/images/chat/live.png';
+import chat1 from '@/images/chat/live1.png';
+import chat2 from '@/images/chat/live2.png';
+import snk from '@/images/snk.png';
+import snk1 from '@/images/snk1.png';
+
+
+export const HomeDatas = {
+    'header': {
+        'topics': [{ 'name': 'About Me', 'slug': '/' }, { 'name': 'Tech Skills', 'slug': '/tech' }, { 'name': 'Projects', 'slug': '/projects' }],
+        'fb': { 'url': 'https://www.facebook.com/ganbold.gantor.771' },
+        'instag': { 'url': 'https://www.instagram.com/gantor12' },
+        'wechat': { 'slug': '/wechat' }
+    },
+    'footer': {
+        'context': 'Made with </> by UKN76',
+        'year': '2023'
+    },
+    'introduction': {
+        'topic': 'Hello there! I will introduce myself in brief. I have been coding for over 3 years now. During those years I managed to do some tiny cool projects, so as to improve my hardware skills. I first enjoyed coding at 16 ^^. You will see the timeline of my coding journey below. Have fun exploring!'  
+    },
+    'year2022': {
+        'id': '2022',
+        'context': 'I made some modifications to the (Mongolian) LEAD ALUMNI ASSOCIATION website as a full- stack developer role and a team lead. Our main goal was to improve our skills and to develop the given website with everything set- up within a deadline. During this year, I learned some basics of frameworks and programming languages: 1. Next.JS, 2. GraphQL, 3. TypeScript, 4. JavaScript ES6, 5. Styled Components, 6. TailwindCSS',
+        'images': [leadPage, leadList, leadLogo]
+    },
+    'year2021': {
+        'id': '2021',
+        'context': `This year I concentrated on learning the basics of databases and web language. Before working on our project, we initially faced some confusion about structuring our data and managing multiple tasks. However, Trello's platform features proved to be invaluable in helping us save time and begin with a well-organized plan. Throughout the development process, working as a team lead was both enjoyable and fulfilling. I aimed to inspire our team of developers by fostering a culture of respect and collaboration. The experience was not only fun but also highlighted the importance of effective leadership in maintaining a positive and productive development atmosphere.`,
+        'images': [shagai, figma, multi]
+    },
+    'year2019': {
+        'id': '2019',
+        'context': `The year when I first started coding web languages on my mobile phone. It all started with an app called mimo. From HTML , CSS , JS to Algorithm problem solving. It was a fun experience to do some little handy project for myself. After gaining some knowledge , I was unsure about what to start next. Then one day while surfing around Facebook, I found Nest Academy randomly. I had no idea about its academy until participating in a coding test lesson by myself. Ever since the participation day, I knew it would help me in my future career.`,
+        'images': [multip, calculator, beacon, snake]
+    },
+    'projects': [{
+        'title': 'LEAD Association of Mongolia',
+        'link': 'https://lead-seven.vercel.app/',
+        'images': [lead, lead1, lead2, lead3, lead4, lead5],
+    }, {
+        'title': 'Хурдан шагай',
+        'link': 'https://last-racer.web.app/',
+        'images': [fast3, fast2, fast1, fast, fast4],
+    },
+    {
+        'title': 'Хурдан шагай - (demo)',
+        'link': 'https://fhprj-42d60.web.app/',
+        'images': [demo, demo1, demo2]
+    },
+    {
+        'title': 'Boginoo - short url generator',
+        'link': 'https://project-delta01.web.app/',
+        'images': [short, short1],
+    },
+    {
+        'title': 'Live - Chat',
+        'link': 'https://react-faeha.web.app/',
+        'images': [chat, chat1, chat2]
+    },
+    {
+        'title': 'Snake game',
+        'link': 'https://tester-4910c.web.app/',
+        'images': [snk, snk1]
+    }
+    ]
+};
